187f5dbdc08bb44fc98a2fd053d702cdf9f34dd4
Evan Pavone <evanrpavone@gmail.com>
2020-08-11 20:22:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,17 +1,23 @@
 require 'pry'
+
+
 class Artist
-  extend Memorable::ClassMethods
-  include Memorable::InstanceMethods
-  extend Findable
-  include Paramable
   attr_accessor :name
   attr_reader :songs
+
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable
 
   @@artists = []
 
   def initialize
     super
     @songs = []
+  end
+
+  def self.find_by_name(name)
+    @@artists.detect{|a| a.name == name}
   end
 
   def self.all
@@ -23,8 +29,7 @@ class Artist
     song.artist = self
   end
 
-  def add_songs(song)
-    songs.each {|song| add_song(song)}
+  def add_songs(songs)
+    songs.each { |song| add_song(song) }
   end
-
 end
